be84dc8f430251a775d2970a8695a9a3953b5d25
Janetzy Maldonado <s23120227@alumnos.itsur.edu.mx>
2026-09-23 21:43:06 -0600

    Commit 3: Implementacion de estado mutable para el contador de agua

--- app/src/main/java/com/example/wellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wellnessapp/MainActivity.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.wellnessapp.ui.theme.WellnessAppTheme
@@ -36,8 +40,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp).padding(top = 40.dp)) {
-        Text(text = "You've had 0 glasses.")
-        Button(onClick = { }, modifier = Modifier.padding(top = 8.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
             Text("Add one")
         }
     }
